930a1c523791a2984e67845c2c300df78d272eb8
jjergus <jjergus@fb.com>
2020-07-31 13:40:05 -0700

    auto_prepend_file __init_autoload.php

---
+++ src/utils/_private/__init_autoload.php
@@ -0,0 +1,15 @@
+<?hh
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+function __init_autoload(): void {
+  require_once __DIR__.'/../../../vendor/autoload.hack';
+  \Facebook\AutoloadMap\initialize();
+}
